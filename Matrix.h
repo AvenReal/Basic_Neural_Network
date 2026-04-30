e6f37581e7a177de659cb637025743ebf258c9e0
Aven <evan.rieber@gmail.com>
2026-04-30 23:56:53 +0900

    added matrix addition, modified matrix multiplications

--- Matrix.h
+++ Matrix.h
@@ -4,6 +4,8 @@
 
 #ifndef BASIC_NEURAL_NETWORK_MATRIX_H
 #define BASIC_NEURAL_NETWORK_MATRIX_H
+#include <format>
+#include <stdexcept>
 
 
 class Matrix {
@@ -23,7 +25,6 @@ class Matrix {
             }
         }
 
-
         ~Matrix() {
             for (int i = 0; i < Height; i++) {
                 delete[] matrix[i];
@@ -32,23 +33,38 @@ class Matrix {
         }
 
 
-        Matrix* operator*(const Matrix* other) {
-            if (Height != other->Width)
-                return nullptr;
+        Matrix operator*(const Matrix& other) {
+            if (Height != other.Width)
+                throw std::runtime_error( std::format( "Matrix Multiplication: Invalid dimensions: (%d, %d) * (%d, %d)", Width, Height, other.Width, other.Height) );
 
-            Matrix* result = new Matrix(Width, other->Height);
+            Matrix result = Matrix(Width, other.Height);
 
-            for (int h = 0; h < result->Height; ++h) {
-                for (int w = 0; w < result->Width; ++w) {
+            for (int h = 0; h < result.Height; ++h) {
+                for (int w = 0; w < result.Width; ++w) {
                     double n = 0;
-                    for (int y = 0; y < result->Height; ++y) {
-                        for (int x = 0; x < result->Width; ++x) {
+                    for (int y = 0; y < result.Height; ++y) {
+                        for (int x = 0; x < result.Width; ++x) {
                             n += matrix[h][x] * matrix[y][w];
                         }
                     }
-                    result->matrix[h][w] = n;
+                    result.matrix[h][w] = n;
                 }
             }
+            return result;
+        }
+
+        Matrix operator+(const Matrix& other) {
+            if (Height != other.Height || Width != other.Width)
+                throw std::runtime_error( std::format( "Matrix Addition: Invalid dimensions: (%d, %d) + (%d, %d)", Width, Height, other.Width, other.Height));
+
+            Matrix result = Matrix(Width, Height);
+
+            for (int i = 0; i < Height; ++i) {
+                for (int j = 0; j < other.Width; ++j) {
+                    result.matrix[i][j] = matrix[i][j] + other.matrix[i][j];
+                }
+            }
+
             return result;
         }
 };
